6502bd990e6df69f7cf2638988de1906184e00f1
AleksandraMN <al_mn@mail.ru>
2022-09-02 21:25:12 +1000

    Homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,80 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+using System;
+class Program
+{
+static void FillArray(int[,] matr)
+{
+	for(int i = 0; i < matr.GetLength(0); i++)  
+	{
+		for(int j = 0; j < matr.GetLength(1); j++) 
+		{
+			matr[i, j]= new Random().Next(1, 11); 
+		}
+	}
+}
+static void PrintArray(int[,] matr)
+{
+		for(int i = 0; i < matr.GetLength(0); i++)  
+	{
+			for(int j = 0; j < matr.GetLength(1); j++) 
+		{
+			Console.Write($"{matr[i, j]} " + "\t ");
+		}
+		Console.WriteLine();
+	}
+}
+static void SelectionSort(int [,] array)
+{
+	for(int i = 0; i < array.GetLength(0); i++)
+	{ 
+			for(int k= 0; k < array.GetLength(1); k++)
+		{
+			int maxPosition = 0;
+			for(int j = k+1; j < array.GetLength(1); j++)
+			{
+				if(array[i,j] > array[i,k])
+				{
+				maxPosition = j;
+				int temporary = array[i,j];	
+				array[i,j] = array[i,k];
+				array[i, k] = temporary;
+				}	
+			}	
+		}
+	}
+}
+
+static void Main(string[] args)
+{
+	Console.WriteLine();
+	Console.Write("Введите количество строк двумерной матрицы: m = ");
+	int m = Convert.ToInt32(Console.ReadLine()); 
+	Console.Write("Введите количество столбиков двумерной матрицы: n = ");
+	int n = Convert.ToInt32(Console.ReadLine());
+
+	int[,] matrix = new int [m, n];
+	Console.WriteLine();
+	Console.WriteLine("Задан массив: ");
+	FillArray(matrix);
+	PrintArray(matrix);
+		
+	Console.WriteLine("Упорядочены по убыванию элементы каждой строки двумерного массива: ");
+	SelectionSort(matrix);
+	PrintArray(matrix);
+}
+}
+
+
+
+
